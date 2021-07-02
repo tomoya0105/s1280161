154d4e419036fcb6962368bac7225e81e33aa654
tomoya0105 <89tomoya0105@gmail.com>
2021-07-02 11:47:09 +0900

    Won/lost message added

--- dice.cpp
+++ dice.cpp
@@ -13,8 +13,11 @@ int main()
 
     int dice1 = rand6(mt1);
     int dice2 = rand6(mt2);
+    int dice_sum = dice1 + dice2;
 
     cout << "Die 1: " << dice1 << endl;
     cout << "Die 2: " << dice2 << endl;
-    cout << "Total value: " << dice1 + dice2 << endl;
+    cout << "Total value: " << dice_sum << endl;
+    if(dice_sum > 7) cout << "You won!" << endl;
+    else cout << "You lost!" << endl;
 }
